27ea8fd4b6bcab9461f29dce140a25aef0049cd6
QueenMae <maena.brice@epitech.digital>
2024-10-01 17:39:24 +0200

    ex_06.js

---
+++ ex_06.js
@@ -0,0 +1,14 @@
+fruits = ["Blackberries", "Apple", "Banana", "Mango", "Peaches", "Kiwi"];
+let fruitName = "Banana"
+function findFruitIndex (fruitName) {
+		const index = fruits.indexOf(fruitName);
+		if (index === -1) {
+		return null;
+		} else {
+		return index;
+		}	
+}
+function disdis(result) {
+displayResult(result);
+}
+disdis(findFruitIndex(fruitName));

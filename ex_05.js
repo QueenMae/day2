9ab661617416976c64de46b364327034ca7859e7
QueenMae <maena.brice@epitech.digital>
2024-10-01 14:27:10 +0200

    ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,13 @@
+function getBasketContent() {
+    const fruits = [
+"strawberry", "strawberry", "strawberry","apple-1","apple-1","lime","lime","peach","pear","pear"
+
+       
+    ]
+
+    console.log(fruits.length + " fruit(s) selected");
+    
+    return fruits;
+}
+
+getBasketContent();
